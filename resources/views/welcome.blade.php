d86802ea650ccf59d0f5871518ca5f3c98edfb60
Sylwester <sylwester@marte.best>
2024-04-30 13:35:45 +0200

    #22 Update. Dropdowns fix2 Bootstrap 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,12 +16,12 @@
                                 <label class="mr-2">Sort by:</label>
                                 
                                <a class="btn btn-lg btn-light dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="#" >Relevance <span class="caret"></span></a>
-                                <ul  class="dropdown-menu" aria-labelledby="navbarDropdown" x-placement="bottom-start" style="position: absolute; transform: translate3d(71px, 48px, 0px); top: 0px; left: 0px; will-change: transform;">
-                                    <li><a class="dropdown-item" href="#">Relevance</a></li>
-                                    <li><a class="dropdown-item" href="#">Price Descending</a></li>
-                                    <li><a class="dropdown-item" href="#">Price Ascending</a></li>
-                                    <li><a class="dropdown-item" href="#">Best Selling</a></li>
-                                </ul>
+                                <div  class="dropdown-menu" aria-labelledby="navbarDropdown" x-placement="bottom-start" style="position: absolute; transform: translate3d(71px, 48px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                    <a class="dropdown-item" href="#">Relevance</a>
+                                    <a class="dropdown-item" href="#">Price Descending</a>
+                                    <a class="dropdown-item" href="#">Price Ascending</a>
+                                    <a class="dropdown-item" href="#">Best Selling</a>
+                                </div>
                             </div>
                             
                             <div class="btn-group">
