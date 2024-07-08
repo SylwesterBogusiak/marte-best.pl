b5745d05ef6a7468cfe62011b11a5ef12c7e570b
Sylwester <sylwester@marte.best>
2024-07-08 14:14:21 +0200

    #58 CSRF and ajax

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,14 @@ window.$ = jQuery;
 
 import Swal from 'sweetalert2'
 window.Swal = Swal
+
+try {
+    window.$.ajaxSetup({
+        headers:{
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+
+    });
+    
+    
+} catch (e) {}
